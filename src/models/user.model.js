3d3b383072230d7a7ac7c000740c82f303a0521d
LepharamRamchiary <lepharamchiary@gmail.com>
2025-02-08 13:48:43 +0530

    add user model

---
+++ src/models/user.model.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true, 
+    },
+    fullname: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    avatar: {
+        type: String,
+        required: true
+    },
+    refreshToken: {
+        type: String
+    }
+}, { timestamps: true });
+
+export const User = mongoose.model("User", userSchema);
