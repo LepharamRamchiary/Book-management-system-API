5d74d5270e6bdf04720aa1f42928d606c8a89c9e
LepharamRamchiary <lepharamchiary@gmail.com>
2025-02-17 11:58:39 +0530

    add hashing password to normal password

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,42 +1,56 @@
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
-const userSchema = new mongoose.Schema({
+const userSchema = new mongoose.Schema(
+  {
     username: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true,
-        index: true,
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
     },
     email: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true, 
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
     },
     fullname: {
-        type: String,
-        required: true,
-        trim: true,
-        index: true
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
     },
     password: {
-        type: String,
-        required: [true, "Password is required"]
+      type: String,
+      required: [true, "Password is required"],
     },
     avatar: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
     refreshToken: {
-        type: String
+      type: String,
     },
     isAdmin: {
-        type: Boolean,
-        default: false,
-    }
-}, { timestamps: true });
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+
 
 export const User = mongoose.model("User", userSchema);
