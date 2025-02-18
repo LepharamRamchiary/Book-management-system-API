3e5f5d6afb7a9a05dbb8258600055aa31b62cbfd
LepharamRamchiary <lepharamchiary@gmail.com>
2025-02-18 14:17:03 +0530

    add get comment endpoint

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -23,4 +23,41 @@ const addComment = asyncHandler(async (req, res) => {
     .json(new ApiResponse(201, comment, "Comment added successfully"));
 });
 
-export { addComment };
+const getBookComments = asyncHandler(async (req, res) => {
+  const { bookId } = req.params;
+  const { page = 1, limit = 10 } = req.query;
+
+  if (!mongoose.Types.ObjectId.isValid(bookId)) {
+    throw new ApiError(400, "Invalid book ID");
+  }
+
+  const comments = await Comment.aggregate([
+    { $match: { book: new mongoose.Types.ObjectId(bookId) } },
+    { $sort: { createdAt: -1 } },
+    {
+      $facet: {
+        metadata: [
+          { $count: "total" },
+          { $addFields: { page: parseInt(page) } },
+        ],
+        data: [{ $skip: (page - 1) * limit }, { $limit: parseInt(limit) }],
+      },
+    },
+    {
+      $unwind: "$metadata",
+    },
+  ]);
+
+  const response = {
+    comments: comments.length > 0 ? comments[0].data : [],
+    totalPages:
+      comments.length > 0 ? Math.ceil(comments[0].metadata.total / limit) : 0,
+    currentPage: parseInt(page),
+  };
+
+  res
+    .status(200)
+    .json(new ApiResponse(200, response, "Comments retrieved successfully"));
+});
+
+export { addComment, getBookComments };
--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { addComment } from "../controllers/comment.controller.js";
+import { addComment, getBookComments } from "../controllers/comment.controller.js";
 
 const router = Router();
 router.use(verifyJWT); // Applying verifyJWT for all routes
 
 router.route("/add-comment/:bookId").post(addComment);
+router.route("/get-comment/:bookId").get(getBookComments);
 
 export default router;
